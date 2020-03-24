7aa2821a2699ba3deae19354afaac131c5865fd7
1332812330@qq.com <1332812330@qq.com>
2020-03-24 20:50:14 +0800

    day03

---
+++ day03/seek.py
@@ -0,0 +1,19 @@
+"""
+文件偏移量
+"""
+
+# 读写方式打开
+f = open('file','wb+')
+
+f.write("Hello world\n".encode())
+f.flush()
+
+# 查看文件偏移量位置
+print("文件偏移位置：",f.tell())
+
+# 操作文件偏移量
+f.seek(-2,2)
+
+print("读取内容：",f.read())
+
+f.close()
